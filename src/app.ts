87ecebe90b7e0cef87de467de72ee9fc95aa17c3
meu pessoal <gabrieldietzenovy@gmail.com>
2024-06-28 15:53:18 -0300

    Added input validations for API data entry

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
-import express from 'express';
-
+import express, { Request, Response, NextFunction } from 'express';
+import bodyParser from 'body-parser';
 import { router } from './routes/SaqueRoutes';
 
 export const app = express();
 
-app.use(express.json());
+// Middleware de body-parser
+app.use(bodyParser.json());
+
+// Middleware para capturar erros de JSON inválido
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof SyntaxError && 'status' in err && err.status === 400 && 'body' in err) {
+        return res.status(400).json({ error: 'JSON inválido' });
+    }
+    next();
+});
+    
 app.use('/api/saque', router);
+
--- src/controllers/SaqueController.ts
+++ src/controllers/SaqueController.ts
@@ -1,9 +1,29 @@
 import { Request, Response } from 'express';
-import {CalculaSaque} from '../Utils/CalculaSaque';
+import { CalculaSaque } from '../Utils/CalculaSaque';
 
 export const SaqueController = {
     async indexAll(req: Request, res: Response): Promise<void> {
         const { valor }: { valor: number } = req.body;
+
+        // Verificação se a chave 'valor' está presente 
+        if (valor === undefined) {
+         res.status(400).json({ error: 'A chave "valor" deve estar presente no corpo da requisição' });
+        } 
+        // Validação do valor
+        else if (typeof valor !== 'number') {
+             res.status(400).json({ error: 'O valor deve ser um número' });
+        } else if (!Number.isInteger(valor)) {
+             res.status(400).json({ error: 'Você não pode sacar um valor que contenha centavos, por favor insira um valor inteiro' });
+        } else if (valor <= 0) {
+             res.status(400).json({ error: 'Você não pode sacar um valor negativo ou igual a zero' });
+        } else if (valor === 1 || valor === 3) {
+             res.status(400).json({ error: 'Moeda de 1 real não disponível, por favor selecione outro valor para saque' });
+        } 
+        //nosso banco não tem tanto dinheiro assim XD, chame o Gabriel para resolver!
+        else if (valor > 50000) {
+             res.status(400).json({ error: 'Por favor consulte o gerente Gabriel Dietze do banco Morada, o valor máximo para saque é de 50000 reais' });
+        } else {
+
         try {
             const resultado = CalculaSaque(valor);
             res.status(200).json({ resultado });
@@ -11,5 +31,5 @@ export const SaqueController = {
             console.error(error);
             res.status(500).json({ error: 'Erro ao processar a solicitação' });
         }
-    }
+    }}
 };
